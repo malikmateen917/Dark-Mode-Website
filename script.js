3fcce3178e57f6ebc3b7f0e1b5c12212da699992
malikmateen917 <92625804+malikmateen917@users.noreply.github.com>
2022-05-25 10:25:45 +0500

    website script.js script src completed

---
+++ script.js
@@ -0,0 +1,10 @@
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "dark theme icon/sun.png";
+    }else{
+        icon.src = "dark theme icon/moon.png";
+    }
+}
